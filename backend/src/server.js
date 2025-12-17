35eafe10403920cf4920d0401898496c9a27bc68
mdarrakibislamrifat <rohanrifat43@gmail.com>
2025-12-17 11:45:36 +0600

    Folder structure manage

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,19 +1,13 @@
 import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.get("/api/auth/signup", (req, res) => {
-  res.send("Signup route");
-});
-
-app.get("/api/auth/login", (req, res) => {
-  res.send("Login route");
-});
-
-app.get("/api/auth/logout", (req, res) => {
-  res.send("Logout route");
-});
+app.use("/api/auth", authRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
